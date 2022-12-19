f0e9674b349e67746bc7af4e58c0b71987e1eb00
Tuan Phung <tuan@phung.cz>
2022-12-19 10:44:44 +0100

    added umami

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,15 @@
 export default function Head() {
   return (
     <>
-      <title></title>
+      <title>JavascriptDevs.com</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
+      <script
+        async
+        defer
+        data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID}
+        src="https://umami-nu.vercel.app/umami.js"
+      ></script>
     </>
-  )
+  );
 }
