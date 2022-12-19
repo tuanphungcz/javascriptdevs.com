93f0d1a13c77510e342463e875c68409d2daaac1
Tuan Phung <tuan@phung.cz>
2022-12-19 14:32:48 +0100

    added title and description to detail page

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,3 +1,14 @@
+import slugify from "slugify";
+import { BlogType } from "../types/supabase";
+import supabase from "./supabase";
+
 export function cn(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
+
+export const getBlogBySlug = async (slug: string) => {
+  const { data: blogs }: any = await supabase.from("blogs").select("*");
+  return blogs.find(
+    (blog: BlogType) => slugify(blog.name.toLowerCase()) === slug
+  );
+};
