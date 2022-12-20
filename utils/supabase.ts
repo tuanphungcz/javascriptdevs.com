61f70c5d4965bb4b3564497eddcc2c9f04804505
Tuan Phung <tuan@phung.cz>
2022-12-20 13:28:47 +0100

    added tooling for profile adding

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export default createClient(
+const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
+
+export default supabase;
