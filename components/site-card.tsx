9d1d492ae5b5df75df8f00969cf0b8f6d76cb38e
Tuan Phung <tuan@phung.cz>
2022-12-31 10:06:40 +0100

    updated detail page

--- components/site-card.tsx
+++ components/site-card.tsx
@@ -1,62 +1,61 @@
 /* eslint-disable @next/next/no-img-element */
 
-import { IconBrandGithub, IconExternalLink } from "tabler-icons";
+import { IconBrandGithub, IconExternalLink, IconStar } from "tabler-icons";
 import { PrimaryButton, SecondaryButton } from "./button";
-import { getGithubUsername, stripUrl } from "../utils/utils";
+import { getGithubUsername } from "../utils/utils";
 import NewTabLink from "./new-tab-link";
 import type { Site } from "@prisma/client";
+import BlurImage from "./blur-image";
 
 export default function SiteCard({ site }: { site: Site }) {
   return (
-    <div className="mx-auto mt-8 max-w-2xl">
-      <div className="rounded-md border border-zinc-300 bg-white p-8">
-        {site.websiteUrl && (
-          <NewTabLink href={site.websiteUrl}>
-            <h3 className="mt-4 flex space-x-2 text-sm text-gray-700">
-              <span>{stripUrl(site.websiteUrl)}</span>{" "}
-              <IconExternalLink className="h-5 w-5" />
-            </h3>
-          </NewTabLink>
-        )}
-        <p className="mt-1 text-2xl font-medium text-gray-900">
-          {site.githubUrl && getGithubUsername(site.githubUrl)}
-        </p>
-        {site.description && (
-          <h3 className="mt-2 flex text-gray-700">{site.description}</h3>
-        )}
-        <div className="flex justify-end space-x-4">
-          {site.githubUrl && (
-            <NewTabLink href={site.githubUrl}>
-              <SecondaryButton>
-                <div className="flex space-x-2">
-                  <IconBrandGithub className="h-5 w-5 text-black" />
-                  <div>Source code</div>
-                </div>
-              </SecondaryButton>
-            </NewTabLink>
+    <>
+      <div className="mx-auto mt-8 max-w-2xl">
+        <div className="mb-8 rounded-md">
+          <h1 className="mt-1 text-2xl font-medium text-gray-900 capitalize">
+            {site.githubUrl && getGithubUsername(site.githubUrl)}
+          </h1>
+          {site.description && (
+            <p className="mt-2 flex text-sm text-gray-700">
+              {site.description}
+            </p>
           )}
-          {site.websiteUrl && (
-            <NewTabLink href={site.websiteUrl}>
-              <PrimaryButton>
-                <div className="flex space-x-2">
-                  <div>Visit site</div>
-                  <IconExternalLink className="h-5 w-5 text-white" />
-                </div>
-              </PrimaryButton>
-            </NewTabLink>
-          )}
-        </div>
-      </div>
 
-      {site.imageUrl && (
-        <div className="my-8">
-          <img
-            className="w-screen rounded-md border border-zinc-300  object-cover shadow-md"
-            src={site.imageUrl}
-            alt=""
-          />
+          <div className="mt-4">
+            {site?.stargazersCount && (
+              <div className="inline-flex items-center justify-between space-x-2">
+                <IconStar className="h-4 w-4" />
+                <div className="text-xs text-gray-700">
+                  {site.stargazersCount}
+                </div>
+              </div>
+            )}
+          </div>
+          <div className="mt-4 flex justify-end space-x-4">
+            {site.githubUrl && (
+              <NewTabLink href={site.githubUrl}>
+                <SecondaryButton>
+                  <div className="flex space-x-2">
+                    <IconBrandGithub className="h-5 w-5 text-black" />
+                    <div>Source code</div>
+                  </div>
+                </SecondaryButton>
+              </NewTabLink>
+            )}
+            {site.websiteUrl && (
+              <NewTabLink href={site.websiteUrl}>
+                <PrimaryButton>
+                  <div className="flex space-x-2">
+                    <div>Visit site</div>
+                    <IconExternalLink className="h-5 w-5 text-white" />
+                  </div>
+                </PrimaryButton>
+              </NewTabLink>
+            )}
+          </div>
         </div>
-      )}
-    </div>
+        <div>{site.imageUrl && <BlurImage src={site.imageUrl} />}</div>
+      </div>
+    </>
   );
 }
--- pages/site/[slug].tsx
+++ pages/site/[slug].tsx
@@ -2,11 +2,17 @@ import type { Site } from "@prisma/client";
 import slugify from "slugify";
 import Container from "../../components/container";
 import SiteCard from "../../components/site-card";
+import Link from "next/link";
 
 import { prisma } from "../../server/db/client";
 import { getSiteBySlug, getGithubUsername, stripUrl } from "../../utils/utils";
+import { trpc } from "../../utils/trpc";
+import BlurImage from "../../components/blur-image";
+import { IconStar } from "tabler-icons";
 
 export default function Page({ site }: { site: Site }) {
+  const { data: sites } = trpc.site.get6RandomActive.useQuery();
+
   if (!site) {
     return null;
   }
@@ -29,8 +35,55 @@ export default function Page({ site }: { site: Site }) {
         data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID}
         src="https://umami-nu.vercel.app/umami.js"
       ></script>
+      <div className="bg-gray-100 pt-4 pb-16">
+        <Container>
+          <SiteCard site={site} />
+        </Container>
+      </div>
       <Container>
-        <SiteCard site={site} />
+        <div className="mx-auto max-w-7xl py-16">
+          <h2 className="mb-2 text-xl font-medium text-gray-800">
+            You might also like
+          </h2>
+          <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 md:grid-cols-3 xl:gap-x-8">
+            {sites?.map((site: Site) => (
+              <Link
+                key={site.id}
+                href={`/site/${slugify(
+                  (site.websiteUrl && stripUrl(site.websiteUrl)) || "no-url"
+                )}`}
+                className="group"
+              >
+                {site.imageUrl && <BlurImage src={site.imageUrl} />}
+                <div className="mt-4 flex items-center justify-between">
+                  <h3 className=" text-sm text-gray-700">
+                    By {site.githubUrl && getGithubUsername(site.githubUrl)}
+                  </h3>
+                  {site?.stargazersCount && (
+                    <div className="flex items-center justify-between space-x-2">
+                      <IconStar className="h-4 w-4" />
+                      <div className="text-xs text-gray-700">
+                        {site.stargazersCount}
+                      </div>
+                    </div>
+                  )}
+                </div>
+
+                <h1 className="mt-2 mb-4 text-lg font-medium text-gray-900">
+                  {site.websiteUrl && stripUrl(site.websiteUrl)}
+                </h1>
+
+                {site.description && (
+                  <p className="text-sm text-gray-700">
+                    {site.description?.length > 80
+                      ? site.description?.slice(0, 80) + "..."
+                      : site.description}
+                  </p>
+                )}
+              </Link>
+            ))}
+          </div>
+        </div>
       </Container>
     </>
   );
